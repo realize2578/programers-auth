d89883de981cb8215b5ca8b265b799046bc77770
jinwoo <realize2578@gmail.com>
2026-04-02 15:12:39 +0900

    로깅

--- src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -15,7 +15,7 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("hihi");
+        logger.debug("Custom Authentication Filter is Called");
         filterChain.doFilter(request, response); // 이거 안해주면 인가? 인증? 도중 멈춤
     }
 }
